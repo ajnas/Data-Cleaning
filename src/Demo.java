ca660faa8462d1e7fa723a882e8af78513bf0413
Ajnas <mailajnaskt@gmail.com>
2015-05-21 16:20:50 +0530

    Inputs inside the program

--- src/Demo.java
+++ src/Demo.java
@@ -1,14 +1,34 @@
 import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.Scanner;
 
 class Demo {
     public static void main(String[] args) {
-    	String server=args[0];
-    	String dbName=args[1];
-    	String user=args[2];
-    	String pass=args[3];
-    	String table=args[4];
-    	String attribute=args[5];
+    	String server;
+    	String dbName;
+    	String user;
+    	String pass;
+    	String table;
+    	String attribute;
+    	double disThreshold,occRel;
+    	disThreshold=6.0;
+    	Scanner in = new Scanner(System.in);
+//        System.out.println("Enter value of disThreshold");
+//        disThreshold= in.nextDouble();
+         System.out.println("Enter server name");
+         server = in.nextLine();
+         System.out.println("Enter name of the database");
+         dbName = in.nextLine();
+         System.out.println("Enter username for database");
+         user = in.nextLine();
+         System.out.println("Enter password ");
+         pass = in.nextLine();
+         System.out.println("Enter table name");
+         table = in.nextLine();
+         System.out.println("Enter name of attribute to be cleaned");
+         attribute = in.nextLine();
+         System.out.println("Enter disThreshold Value");
+         disThreshold = in.nextDouble();
         MySQLAccess dao = new MySQLAccess();
         try {
 			dao.connnectDataBase(server,dbName,user,pass);
@@ -17,14 +37,22 @@ class Demo {
 			e.printStackTrace();
 		}
         
+        
+        
+        
         ArrayList<String> input=dao.getValues(table,attribute);
+        System.out.println("Number of distinct values before cleaning = "+input.size());
+        
+      
         if(input==null)
         	return;
-        Cleaning cleaning=new Cleaning(input,6.0,1.0);
+        Cleaning cleaning=new Cleaning(input,disThreshold,1.0);
         cleaning.execute();
         
         dao.applyCleaning(cleaning.getClusters(),table,attribute);
         System.out.println(cleaning.getOutputString());
+        System.out.println("Number of distinct values after clening = "+cleaning.getClusters().size());
+
         
     }
     
